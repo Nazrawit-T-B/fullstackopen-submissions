6d87c53bcc27636a785cafa3869926a585149c80
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-07-29 13:28:48 +0300

    unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,36 @@
 import {useState} from 'react'
+const Stat=({good,neutral, bad,all})=>{
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good-bad)/all}</p>
+      <p>positive {(good/all)*100} %</p>
+    </>
+  )
+
+}
+
 const App=()=>{
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll]=useState(0)
+  const[avg,setAvg]=useState(0)
   const handleGoodClick=()=>{
     setGood(good+1)
+    setAll(all+1)
   }
   const handleNeutralClick=()=>{
     setNeutral(neutral+1)
+    setAll(all+1)
   }
   const handleBadClick=()=>{
     setBad(bad+1)
+    setAll(all+1)
   }
   return (
     <div>
@@ -19,11 +39,11 @@ const App=()=>{
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+      
     </div>
   )
 }
+
 
 export default App
