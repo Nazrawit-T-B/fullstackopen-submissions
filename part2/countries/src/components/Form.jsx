a664f820037ae041ee7ba7f7729c9dc61b1422b4
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-08-12 09:31:11 +0300

    initalize part2 countries exercise

---
+++ part2/countries/src/components/Form.jsx
@@ -0,0 +1,11 @@
+const Form = ({}) => {
+  return (
+    <form >
+      <div>
+        find countries:  
+        <input className="input-field" />
+      </div>
+    </form>
+  );
+};
+export default Form;
