cb7ca72ddee28a19d643b1de4f4b9cacbee21053
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-08-13 22:00:27 +0300

    countries step 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -10,13 +10,16 @@ const App = () => {
   const countriesToShow = countries.filter((country) =>
     country.name.common.toLowerCase().includes(searchFilter.toLowerCase()),
   );
+  const handleView=(country)=>{
+   setSearchFilter(country.name.common)
+  }
   return (
     <div>
       <Form searchFilter={searchFilter} setSearchFilter={setSearchFilter} />
       <div>
         {
          countriesToShow.length >10? (  <p>Too many matches, specify another filter</p>): countriesToShow.length >1? (countriesToShow.map(country => (
-          <p key={country.cca3}>{country.name.common}</p>
+          <p key={country.cca3}>{country.name.common} <button onClick={()=>handleView(country)}>Show</button></p>
         ))):countriesToShow.length === 1 ? (<div>
           <h1>{countriesToShow[0].name.common}</h1>
           <p>Capital {countriesToShow[0].capital}</p>
